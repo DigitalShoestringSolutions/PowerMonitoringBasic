87f9c270577667810445c14b897c00232d398067
Greg-Hawkridge <ghawkridge@gmail.com>
2023-05-05 12:42:45 +0100

    Fixes log rotation

--- logging/setup_logging.sh
+++ logging/setup_logging.sh
@@ -4,12 +4,13 @@ echo "Setting Up Logging"
 echo "---------------------"
 sudo apt-get install rsyslog logrotate -y
 
-sudo install -d -m 775 -o root -g adm /var/log/containers
+sudo install -d -m 755 -o root -g adm /var/log/containers
 
 sudo cp setup_files/syslog.conf /etc/rsyslog.d/40-docker.conf
 sudo systemctl restart rsyslog
 
 sudo cp setup_files/logrotate /etc/logrotate.d/docker
+sudo mv /etc/cron.daily/logrotate /etc/cron.hourly/logrotate
 
 echo "---------------------"
 echo "Setup Script Complete"
